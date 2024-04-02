8ad27d3918c12f6489cf47082b0c1b0333858453
Oscar Morales <157633781+OscarRuben2024@users.noreply.github.com>
2024-04-01 22:12:19 -0600

    Update Conexion.java

--- TareasProgra3/ProyectoUMG/src/main/java/datos/Conexion.java
+++ TareasProgra3/ProyectoUMG/src/main/java/datos/Conexion.java
@@ -5,7 +5,7 @@ import java.sql.*;
 public class Conexion {
     private static final String JDBC_URL = "jdbc:mysql://localhost/didactica?useSSL=false&serverTimezone=UTC";
     private static final String JDBC_USER = "root";
-    private static final String JDBC_PASS = "Mastermaster2133";
+    private static final String JDBC_PASS = "Pw";
    
     
     public static Connection getConnection() throws SQLException {
